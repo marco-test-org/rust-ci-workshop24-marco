fb97c86ee07098edadce42a35ace51fc7dd89170
Marco Ieni <11428655+marcoieni@users.noreply.github.com>
2025-03-10 19:28:09 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,5 @@
-#[allow(dead_code)]
-
-/// Just a point
-pub struct Point {
-    x: i64,
-    y: i64,
-}
-
-/// Adds two [`Point`]s together.
 pub fn add() {
-    let var1 = Some(1);
-    if let Some(num) = var1 {
-        println!("{num}")
-    }
+    println!("hello");
 }
 
 #[cfg(test)]
